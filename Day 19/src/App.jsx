c6b9711a9d0690042c26174e083fac4094999d5a
Anjaliparul12 <70659084+Anjaliparul12@users.noreply.github.com>
2021-07-14 11:48:26 +0530

    Counter

--- Day 19/src/App.jsx
+++ Day 19/src/App.jsx
@@ -2,12 +2,15 @@ import Counter from './counter';
 
 function App(){
   return (
+    <div className="heading">
+    <h1>Counter</h1>
     <section>
     <Counter/>
     <Counter/>
     <Counter/>
     <Counter/>
     </section>
+    </div>
   );
 }
 
---
+++ day20/src/components/Card.jsx
@@ -0,0 +1,13 @@
+const Card = (props) =>{
+    return(
+//  <div className="container">
+  <div class="Card">
+  <h2 className="title">{props.title}</h2>
+  <p>you have consumed {props.amount} cals today</p>
+  <button>Delete</button>
+  </div>
+  // </div>
+);
+}
+
+export default Card;
